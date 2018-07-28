ae94cd459369d7bf4f58150402be87521022c97a
Kubilay Şener <kblysnr2050@gmail.com>
2018-07-29 01:12:24 +0300

    Commit

--- app.js
+++ app.js
@@ -135,4 +135,74 @@
 //End Of The Lecture.
 
 //Numbers & Math Object
+      /*   const num1 = 100;
+        const num2 = 50;
+        let val; */
+        // Math Object
+  /*       val = Math.PI;
+        val = Math.E;
+        val = Math.round(2.4);
+        val = Math.ceil(2.4); // round upper
+        val = Math.floor(2.8); // round lower
+        val = Math.sqrt(64);
+        val = Math.abs(-3);
+        val = Math.pow(8, 2); // power of a number
+        val = Math.min(2,35,48,78,5,1,25,-10);
+        val = Math.max(2,35,48,78,5,1,25,-10);
+        val = Math.random();
 
+        val = Math.floor( Math.random()*20 + 1); // Generates number between 1 and 20 includes this numbers;
+        console.log(val); */
+
+//End Of The Lecture.
+
+// Lecture String Method & Concatenation
+      /*   const firstname= 'William';
+        const lastname = 'Jhonson';
+        const age = 27;
+        const str = 'Hello my name is Erşan Kuneri. And I am not a Phornographer.'
+        let val;
+        val = firstname + lastname;
+        
+        // Concatenation
+            val = firstname + ' ' + lastname;
+        
+        // Append
+            val = 'Kubilay ';
+            val +='Şener';
+
+            val = "My name is " + val + ' and I m ' + age + ' years old.'
+        // Escaping
+            val = 'That\'s awesome, I can\'t wait';
+        // Lengt
+            val = firstname.length;
+        // Concat
+            val = firstname.concat(' ',lastname);
+        // Change Case
+            val = firstname.toLowerCase();
+            val = firstname.toUpperCase();
+
+            val = firstname[2];
+        // indexOf()
+            val = firstname.indexOf(1);
+            val = firstname.lastIndexOf(1);
+        // charAt();
+            val = firstname.charAt(0);
+        // Get last char.
+            val = firstname.charAt(firstname.length-1);
+        // substring()
+            val = firstname.substring(0,3);
+        // slice()
+            val = firstname.slice(0,4);
+            val = firstname.slice(-3);
+        // split()
+            val = str.split(' ');
+        // replace()
+            val = str.replace('Erşan','Kubilay');
+        //includes()
+            val = str.includes('Kuneri');
+        console.log(val); */
+//End Of The Lecture.
+
+// Lecture Template Literals
+        
