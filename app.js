2647916c5f343bfc43cc62d673044da9e200236f
Kubilay Şener <kblysnr2050@gmail.com>
2018-07-28 18:09:14 +0300

    Commit

--- app.js
+++ app.js
@@ -1,62 +1,97 @@
 // Lecture Using The Console.
 
-/*
-// alert("ALert");
-console.log("Logged Some Data");
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1,b:2});
+        /*
+        // alert("ALert");
+        console.log("Logged Some Data");
+        console.log(123);
+        console.log(true);
+        var greeting = 'Hello';
+        console.log(greeting);
+        console.log([1,2,3,4]);
+        console.log({a:1, b:2});
+        console.table({a:1,b:2});
 
-console.error('Surprise Mother Fucker');
+        console.error('Surprise Mother Fucker');
 
-console.warn('Watch out mother fucker.');
-console.time('Time');
-    console.log('So fast, look at the time we have been spend!!');
-console.timeEnd('Time');
-console.clear();
+        console.warn('Watch out mother fucker.');
+        console.time('Time');
+            console.log('So fast, look at the time we have been spend!!');
+        console.timeEnd('Time');
+        console.clear();
 
-*/
+        */
 // End Of The Lecture.
 
 //Lecture Variables - var, let & const
 
-// var name = 'John Wick';
-// console.log(name);
-// name = 'Baba Yaga';
-// console.log(name);
+        /* 
+        var name = 'John Wick';
+        console.log(name);
+        name = 'Baba Yaga';
+        console.log(name); 
+        */
+    //init var
+        /* var greeting;
+        console.log(greeting);
+        greeting = 'Hello';
+        console.log(greeting); */
+    // LET
+        /* let isim = 'John Wick';
+        console.log(isim);
+        isim = 'Baba Yaga';
+        console.log(isim); */
 
-//   //init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
+    // CONST 
+        /* const _name = 'John Wick';
+        console.log(_name);
+        You can't re-assing _name.
+        const Name = {
+            firstname :'John',
+            secondname :'Wick',
+        }
+        console.log(Name);
+        Name.firstname = 'Baba';
+        Name.secondname = 'Yaga';
+        console.log(Name) */
+    //But YOU CAN re-assing inside of an object.
+        /* 
+        const numbers = [1,2,3,4,5];
+        numbers.push(6);
+        console.log(numbers); */
 
-//   //LET
+// End Of The Lecture.
 
-// let isim = 'John Wick';
-// console.log(isim);
-// isim = 'Baba Yaga';
-// console.log(isim);
+// Data types in JAVASCRIPT
+    //Primitive
+        /* //String
+        const name = 'John Wick';
+        console.log(typeof name);
+        //Number
+        const age = 27;
+        console.log(typeof age);
+        //Boolean
+        const hasKids = true;
+        console.log(typeof hasKids);
+        //Null
+        const car = null;
+        console.log(typeof car);
+        //Undefined
+        let test;
+        console.log(typeof test);
+        //Reference Types - Objects
+        //Array
+        const Hobbies = ['Movies', 'Gaming'];
+        console.log(typeof Hobbies);
+        //Object Literal
+        const address={
+            city:'İzmir',
+            state:'Republic'
+        }
+        console.log(typeof address);
+        const today = new Date();
+        console.log(typeof today); */
+// End Of The Lecture.
 
-// CONST // 
-// const _name = 'John Wick';
-// console.log(_name);
-// You can't re-assing _name.
-const Name = {
-    firstname :'John',
-    secondname :'Wick',
-}
-console.log(Name);
-Name.firstname = 'Baba';
-Name.secondname = 'Yaga';
-console.log(Name)
-//But YOU CAN re-assing inside of an object.
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-console.log(numbers);
-console.log('Test');
+// Lecture Type Conversion
+        
+    
